c63353142f4a07aed0d2e62e4f487ecb9af63b4c
David Sexton <Dsexton@gus.pittstate.edu>
2018-10-31 23:23:33 -0500

    Made the Skills Page look better. Formatting could still be improved slightly and some functionality still needs added allow selection of the items in the view.

--- App1/App1/SkillsPage.xaml.cs
+++ App1/App1/SkillsPage.xaml.cs
@@ -21,13 +21,55 @@ namespace App1
 
             this.Skills = new ObservableCollection<Skill>();
 
-            Skill skill1 = new Skill
+            Skill skillComputerRepair = new Skill
             {
-                Description = "Test of Description",
-                Name = "Test",
-                Image = "envelope.png"
+                Description = "Gained Experience at Gorilla Geeks",
+                Name = "Computer Repair",
+                Image = "Service.png"
             };
-            this.Skills.Add(skill1);
+            Skill skillUIDesign = new Skill
+            {
+                Description = "Prior Creation of UI's for Applications",
+                Name = "UI Design",
+                Image = "UI.png"
+            };
+            Skill skillAudioEditing = new Skill
+            {
+                Description = "Mixing and Mastering Audio",
+                Name = "Audio Editing",
+                Image = "Audio.png"
+            };
+            Skill skillCustomerService = new Skill
+            {
+                Description = "Provided Help for Countless Clients",
+                Name = "Customer Service",
+                Image = "Face.png"
+            };
+            Skill skillProgrammingExperience = new Skill
+            {
+                Description = "Using C#, Visual Basic, and Robot C",
+                Name = "Programming Experience",
+                Image = "Code.png"
+            };
+            Skill skillCADTechnician = new Skill
+            {
+                Description = "Knowledge of Inventor, SolidWorks and Revit",
+                Name = "CAD Technician",
+                Image = "Cad.png"
+            };
+            Skill skillElectronicsBuilder = new Skill
+            {
+                Description = "Robotics Creations in Arduino",
+                Name = "Electronics Builder",
+                Image = "Bolt.png"
+            };
+            this.Skills.Add(skillComputerRepair);
+            this.Skills.Add(skillUIDesign);
+            this.Skills.Add(skillAudioEditing);
+            this.Skills.Add(skillCustomerService);
+            this.Skills.Add(skillProgrammingExperience);
+            this.Skills.Add(skillCADTechnician);
+            this.Skills.Add(skillElectronicsBuilder);
 
             this.skillsList.ItemsSource = this.Skills;
 
